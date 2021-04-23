544200f5afbef7e54ebfd4e33f6a22b22a77df07
Andreas Rehn <rehn.andreas86@gmail.com>
2021-04-23 09:53:26 +0200

    u-boot: use addresses defined in u-boot
    u-boot/include/configs/sunxi-common.h already defines addresses
    
    Signed-off-by: Andreas Rehn <rehn.andreas86@gmail.com>

--- recipes-bsp/u-boot/files/sun8i/boot.cmd
+++ recipes-bsp/u-boot/files/sun8i/boot.cmd
@@ -1,9 +1,13 @@
-# Print boot source
-itest.b *0x28 == 0x00 && echo "U-boot loaded from SD"
-itest.b *0x28 == 0x02 && echo "U-boot loaded from eMMC or secondary SD"
-itest.b *0x28 == 0x03 && echo "U-boot loaded from SPI"
+# Print and set boot source
+itest.b *0x28 == 0x00 && echo "U-boot loaded from SD" && rootdev="/dev/mmcblk0p2";
+itest.b *0x28 == 0x02 && echo "U-boot loaded from eMMC or secondary SD" && setenv rootdev "/dev/mmcblk1p2";
+# TODO: use ubifs and define needed partions
+itest.b *0x28 == 0x03 && echo "U-boot loaded from SPI" && rootdev="31:03"
 
-setenv bootargs console=ttyS0,115200 root=/dev/mmcblk0p2 rootwait panic=10
-load mmc 0:1 0x43000000 ${fdtfile} || load mmc 0:1 0x43000000 boot/${fdtfile}
-load mmc 0:1 0x42000000 zImage || load mmc 0:1 0x42000000 boot/zImage
-bootz 0x42000000 - 0x43000000
+setenv bootargs console=${console} root=${rootdev} rootwait panic=10 ${extra}
+
+# TODO: fel mode can load dtb and kernel to desired address
+# TODO: handle boot from nor-flash (sf probe)
+load mmc 0:1 ${fdt_addr_r} ${fdtfile} || load mmc 0:1 ${fdt_addr_r} boot/${fdtfile}
+load mmc 0:1 ${kernel_addr_r} zImage || load mmc 0:1 ${kernel_addr_r} boot/zImage
+bootz ${kernel_addr_r} - ${fdt_addr_r} || bootm ${kernel_addr_r} - ${fdt_addr_r}
